981e6700341cdc6e7aa178b294771c450389209e
SSHIVA48 <107909751+SSHIVA48@users.noreply.github.com>
2022-09-09 15:09:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "AKIA5N66TX4NAFHCLGNJ"
-  secret_key = "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"
+  access_key = "*****************"
+  secret_key = "*********************"
 }
